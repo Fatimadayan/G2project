25f0f116b0106b9faa6145852b2ad5c1e3615b89
Baraah mo <baraahelase47@gmail.com>
2025-05-07 12:38:40 +0300

    Complete home page revamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,16 @@ module.exports = {
     },
     plugins: [],
   }
+
+  module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          inknut: ['"Inknut Antiqua"', 'serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
   
