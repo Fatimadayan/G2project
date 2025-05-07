165b0d4ebdca1fa0415bc4a43d1af9164a7a0d79
username <Fatimadayan14@gmail.com>
2025-05-08 02:12:48 +0300

    55

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./*.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          inknut: ['"Inknut Antiqua"', 'serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
+  
+  
